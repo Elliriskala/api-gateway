ada6dd3e6491ade85b6f10600c43370ad322bba8
ellinor <ellinoora.riskala@metropolia.fi>
2025-09-15 13:59:59 +0300

    apigateway

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import app from './app';
 
-const port = process.env.PORT || 3008;
-app.listen(port, () => {
-  console.log(`API Gateway is running on http://0.0.0.0:${port}`);
+const port = Number(process.env.PORT) || 3008;
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`API Gateway is running on http://0.0.0.0:${port} (IPv4)`);
 });
